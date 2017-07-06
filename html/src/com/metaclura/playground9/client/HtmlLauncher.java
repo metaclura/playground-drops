96ca31dbb176f06685446996214f3dcb9a026311
metaclura <metaclura@gmail.com>
2017-07-06 11:18:05 +0200

    html part 1

---
+++ html/src/com/metaclura/playground9/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.metaclura.playground9.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.metaclura.playground.Playground;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(800, 480);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Playground();
+        }
+}
